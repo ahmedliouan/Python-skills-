5364f61a85edf5822f786fff201d3c05429d3f3a
ahmedliouan <166460981+ahmedliouan@users.noreply.github.com>
2024-06-19 19:18:08 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+first=float(input("enter first number:"))
+second=float(input("enter second number:"))
+
+print(first ,"+", second ,"=",first + second)
+print(first ,"-", second ,"=",first - second)
+print(first ,"*", second ,"=",first * second)
+print(first ,"/", second ,"=",first / second)
+
+print("end of program")
